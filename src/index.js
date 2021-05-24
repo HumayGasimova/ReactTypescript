dd5996eda0ac3428f3c5ce5cb8d32c783566cb97
HumayGasimova <41358490+HumayGasimova@users.noreply.github.com>
2021-05-24 10:38:40 +0400

    Init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var react_dom_1 = require("react-dom");
+react_dom_1.default.render(react_1.default.createElement("h1", null, "This is JSX"), document.getElementById('app'));
